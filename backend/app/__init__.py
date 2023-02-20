92439578c9df943461b3774978eff1fd9fae225f
jephdo <jephdo@gmail.com>
2023-02-20 12:28:11 -0800

    Run uvicorn by passing a factory method instead of the app instance itself

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,4 +1,5 @@
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
+
 
 from .api import app
 from .settings import settings
@@ -10,12 +11,26 @@ async def startup():
         print(settings)
 
 
-origins = ["*"]
+def create_app() -> FastAPI:
+    from fastapi.middleware.cors import CORSMiddleware
+    from tortoise.contrib.fastapi import register_tortoise
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+    origins = ["*"]
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    register_tortoise(
+        app,
+        db_url=settings.DATABASE_URL,
+        modules={"models": ["app.models"]},
+        generate_schemas=True,
+        add_exception_handlers=True,
+    )
+
+    return app
